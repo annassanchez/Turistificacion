8721129f19bf05e5738b977857a04e6c27cda3cf
annassanchez <annassanchez@gmail.com>
2023-04-10 10:05:39 +0200

    adding geolocation function

--- src/supportClean.py
+++ src/supportClean.py
@@ -1,5 +1,17 @@
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+
+tqdm.pandas()
+from IPython.display import clear_output
+
+# geolocation
+import geopy
+import geopandas
+from geopy.extra.rate_limiter import RateLimiter
+from functools import partial
+from geopy import Nominatim
+
 
 def columnList(dataframe, condition):
     """
@@ -17,3 +29,22 @@ def columnList(dataframe, condition):
             pass
     print(f'the columns that have {condition} on their name are:', column_list)
     return column_list
+
+def geolocation(df, column):
+    locator = Nominatim(user_agent="myGeocoder")
+
+    # 1 - conveneint function to delay between geocoding calls
+    geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
+    # 2- - create location column
+    try:
+        df['location'] = df[column].progress_apply(geocode)
+    except:
+        clear_output(wait=True)
+        pass
+    # 3 - create longitude, laatitude and altitude from location column (returns tuple)
+    df['point'] = df['location'].progress_apply(lambda loc: tuple(loc.point) if loc else None)
+    # 4 - split point column into latitude, longitude
+    df[['latitude', 'longitude', 'altitude']] = pd.DataFrame(df['point'].tolist(), index=df.index)
+    clear_output(wait=True)
+    df.head(1)
+    return df
