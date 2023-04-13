188543564180197883879457d63aca3ee74c9b9f
annassanchez <annassanchez@gmail.com>
2023-04-13 19:16:46 +0200

    adding bibliotheque support file for data transformations

---
+++ src/bibliotheque.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+## for changing the scraped ratings from tripadvisor
+ratings = {
+    '4,0 de 5 burbujas':4.0,
+    '4,5 de 5 burbujas':4.5,
+    '3,5 de 5 burbujas':3.5,
+    '5,0 de 5 burbujas':5.0,
+    '3,0 de 5 burbujas':3.0,
+    '2,5 de 5 burbujas':2.5,
+    '-1,0 de 5 burbujas':-1.0,
+    '2,0 de 5 burbujas':2.0,
+    '1,0 de 5 burbujas':1.0,
+    '1,5 de 5 burbujas':1.5,
+    '':np.nan,
+    '0 opiniones':0
+}
