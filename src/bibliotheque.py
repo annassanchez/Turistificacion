7ed9e001584f1da3869174282113f78b6f170026
annassanchez <annassanchez@gmail.com>
2023-04-14 08:52:13 +0200

    equivalences added for hotels

--- src/bibliotheque.py
+++ src/bibliotheque.py
@@ -15,3 +15,31 @@ ratings = {
     '':np.nan,
     '0 opiniones':0
 }
+
+## equivalences for the hotels dataframe
+accommodation = {
+    'Alojamientos':'Accommodation'
+}
+
+accommodation_type = {
+    'Hoteles':'Hotels',
+    'Hostales':'Hostel',
+    'Residencias universitarias':'Dorms',
+    'Apartahoteles':'Apartments',
+    'Pensiones':'Hostels',
+    'Albergues':'Hostels',
+    'Camping':'Camping' 
+}
+
+hotel_apartments_ratings = {
+    '4 estrellas':4.0,
+    '2 estrellas':2.0,
+    '3 estrellas':3.0,
+    '1 estrella':1.0,
+    '5 estrellas':5.0,
+    '3 llaves':3.0,
+    '5 estrellas Gran Lujo':6.0,
+    '1 llave':1.0,
+    '4 llaves':4.0,
+    '2 llaves':2.0     
+}
