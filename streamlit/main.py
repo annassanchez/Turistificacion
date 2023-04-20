d47b347a42ba4465571c2fedeafd392203cd584a
annassanchez <annassanchez@gmail.com>
2023-04-20 23:35:30 +0200

    changing main streamlit page

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,31 +1,21 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import folium
-from streamlit_folium import st_folium
-import branca
+import pickle
 import geopandas as gpd
+from streamlit_keplergl import keplergl_static
+from keplergl import KeplerGl
+import src.supportData as sd
 
-st.title('test')
+st.set_page_config(layout="centered", initial_sidebar_state="collapsed", page_title='touristification', page_icon="")
 
 
+gdf_2017_competencia, gdf_2023_competencia = sd.importCompetenciaDelSuelo()
 
-# center on Liberty Bell, add mark
-m2 = folium.Map(
-        location=[40.41694, -3.70361],
-        tiles='cartodbpositron',
-        zoom_start=12,
-    )
+st.title('Touristification')
 
-gdf = gpd.read_file('../data/airbnb_airdna/2023_03_13/neighbourhoods.geojson')
-
-folium.GeoJson(gdf, style_function=lambda feature: {
-    'color': 'black',
-    'weight': 1,
-    'dashArray': '5, 5',
-    'fillOpacity':0.5
-}).add_to(m2)
-
-# call to render Folium map in Streamlit, but don't get any data back
-# from the map (so that it won't rerun the app when the user interacts)
-st_folium(m2, width=725, returned_objects=[])
+st.markdown("""
+    Touristification is a project that aims to create an atlas to measure the impact of tourism in the city of Madrid. 
+    <br>This project is based on the research developed by the architecture studio [300.000 Km/s](http://turistificacion.300000kms.net/) based on multiple data sources: open statistical data (Catastro, Ayuntamiento de Madrid Open Data Platform) and soft-data (scraped or crawled from Tripadvisor and Airbnb).  
+    Their analysis was based on data gathered around 2017, so the aim is to see if something has changed in the city of Madrid. AirBnB listings are growing? The residential house market has been affected? 
+""", unsafe_allow_html = True)
