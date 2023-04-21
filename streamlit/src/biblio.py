d7c37fdb713fd6a93e14ff92b7ca8342d5ae5ebd
annassanchez <annassanchez@gmail.com>
2023-04-21 16:41:58 +0200

    biblio added

---
+++ streamlit/src/biblio.py
@@ -0,0 +1,38 @@
+fiesta = {
+    'abnb_tot_offer':'# of AirBnB listings offered',
+    'local200_n_prox_alcohol':'# of Licquor Stores',
+    'local200_n_restaurants':'# of Restaurants',
+    'local200_n_fiesta':'# of Nightclubs',
+    'local200_n_sex':'# of Sex Shops',
+    'tp_offers_tot':"Tripadvisor's listed restaurants",
+    'abnb_tot_price':'Avg AirBnB price (€)',
+    'cad_tot_hotels':'# Hotels',
+    'cad_tot_houses':'# Dwellings',
+    'stock_airbnb':'Stock AirBnB (AirBnB / Dwellings)',
+}
+
+fiesta_inverted = {
+    '# of AirBnB listings offered':'abnb_tot_offer',
+    '# of Licquor Stores':'local200_n_prox_alcohol',
+    '# of Restaurants':'local200_n_restaurants',
+    '# of Nightclubs':'local200_n_fiesta',
+    '# of Sex Shops':'local200_n_sex',
+    "Tripadvisor's listed restaurants":'tp_offers_tot',
+    'Avg AirBnB price (€)':'abnb_tot_price',
+    '# Hotels':'cad_tot_hotels',
+    '# Dwellings':'cad_tot_houses',
+    'Stock AirBnB (AirBnB / Dwellings)':'stock_airbnb',
+}
+
+category_list = [
+    '# of AirBnB listings offered',
+    '# of Licquor Stores',
+    '# of Restaurants',
+    '# of Nightclubs',
+    '# of Sex Shops',
+    "Tripadvisor's listed restaurants",
+    'Avg AirBnB price (€)',
+    '# Hotels',
+    '# Dwellings',
+    'Stock AirBnB (AirBnB / Dwellings)'
+    ]
